ba95d9e714abb630831fb2a3a437b11d036357b7
sergiy-kyiv-2022 <s22101969@gmail.com>
2023-08-05 19:08:11 +0200

    zdfh

---
+++ js/script.js
@@ -0,0 +1,24 @@
+/*for (let i=0; i<5; i++) {
+    console.log(i);
+    for (let j=0; j<7; j++) {
+        console.log(j);
+    }
+};*/
+
+/*let result ="";
+const length=7;
+for (let i = 1; i<length; i++){
+    for(let j=0; j<i; j++){
+        result += "*";
+    }
+    result +="\n";
+}
+console.log(result);*/
+
+/*function firstTask() {
+    for (let i = 5; i < 11; i++) {
+        console.log(i);
+    }
+}
+firstTask()*/
+
